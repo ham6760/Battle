6eb8fe6c6d10ac1f416254c52889a3e219ef3b2d
Min <ham6760@naver.com>
2024-02-20 08:54:12 +0900

    부트스트랩 템플릿 적용

--- src/main/java/com/bo/battle/HomeController.java
+++ src/main/java/com/bo/battle/HomeController.java
@@ -4,10 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/home")
 public class HomeController {
 
-	@RequestMapping("/home")
+	@RequestMapping("index.do")
 	public String home(){
-		return"index";
+		return "../index";
 	}
 }
